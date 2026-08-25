1dc64860d145e45d166ec5593bcf00d56c959bcc
Nazim6269 <nazimdev10022001@gmail.com>
2026-08-25 15:54:25 +0600

    feat: add login form and credential verification logic

--- includes/auth_functions.php
+++ includes/auth_functions.php
@@ -1,1 +1,56 @@
 <?php
+
+
+function getUsers(string $filePath): array
+{
+
+  if (!file_exists($filePath)) {
+    return [];
+  }
+
+  $jsonContent = file_get_contents($filePath);
+
+  if ($jsonContent == false || $jsonContent == '') {
+    return [];
+  }
+
+  $users = json_decode($jsonContent, true);
+
+
+  if (!is_array($users)) {
+    return [];
+  }
+
+  return $users;
+}
+
+
+function findUserByUserName(array $users, string $username): ?array
+{
+
+  foreach ($users as $user) {
+    if (
+      $user['username'] === $username
+    ) {
+      return $user;
+    }
+  }
+
+  return null;
+}
+
+function verifyLogin(array $users, string $username, string $password): ?array
+{
+  $user = findUserByUserName($users, $username);
+
+  if ($user == null) {
+    return null;
+  }
+
+  if (!password_verify($password, $user['password'])) {
+    return null;
+  }
+
+
+  return $user;
+}
--- login.php
+++ login.php
@@ -3,6 +3,27 @@
 session_start();
 require_once 'includes/auth_functions.php';
 
+$filePath = 'data/users.json';
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $username = trim($_POST['username'] ?? '');
+  $password = $_POST['password'] ?? '';
+
+  $users = getUsers($filePath);
+  $user = verifyLogin($users, $username, $password);
+
+  if ($user !== null) {
+    $_SESSION['user_id'] = $user['id'];
+    $_SESSION['username'] = $user['username'];
+
+    header('Location: dashboard.php');
+    exit;
+  } else {
+    $error = 'Invalid Username or Password';
+  }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -19,7 +40,12 @@ require_once 'includes/auth_functions.php';
 <body>
 
   <h1> Login </h1>
+  <form method="post" action="login.php">
+    <input type="text" placeholder="Username" name="username" required>
+    <input type="password" placeholder="Password" name="password" required>
+    <button type="submit">Login</button>
 
+  </form>
 </body>
 
 </html>
